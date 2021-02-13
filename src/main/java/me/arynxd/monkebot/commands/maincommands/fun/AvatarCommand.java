d090d97e18efeef064069f8d17b676545c17928b
Aryn Folkard <aryn.folkard@null.net>
2021-02-13 17:37:50 +0000

    fixed avatar

--- src/main/java/me/arynxd/monkebot/commands/maincommands/fun/AvatarCommand.java
+++ src/main/java/me/arynxd/monkebot/commands/maincommands/fun/AvatarCommand.java
@@ -7,8 +7,6 @@ import me.arynxd.monkebot.objects.command.CommandEvent;
 import me.arynxd.monkebot.objects.exception.CommandException;
 import me.arynxd.monkebot.util.Parser;
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.User;
 import org.jetbrains.annotations.NotNull;
 
 @SuppressWarnings("unused")
@@ -23,17 +21,13 @@ public class AvatarCommand extends Command
 	@Override
 	public void run(@NotNull List<String> args, @NotNull CommandEvent event, @NotNull Consumer<CommandException> failure)
 	{
-		Message message = event.getMessage();
-		User author = message.getAuthor();
-
-		if(message.getMentionedMembers().isEmpty())
+		if(args.isEmpty())
 		{
 			event.sendMessage(new EmbedBuilder()
-					.setTitle(author.getAsTag() + "'s Avatar")
-					.setImage(author.getAvatarUrl() + "?size=4096"));
+					.setTitle(event.getAuthor().getAsTag() + "'s Avatar")
+					.setImage(event.getAuthor().getAvatarUrl() + "?size=4096"));
 			return;
 		}
-
 		new Parser(args.get(0), event).parseAsUser(user ->
 				event.sendMessage(new EmbedBuilder()
 						.setTitle(user.getAsTag() + "'s Avatar")
