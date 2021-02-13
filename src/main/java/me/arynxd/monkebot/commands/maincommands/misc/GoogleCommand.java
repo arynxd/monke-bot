651cd6297dc4ebc49f4a89fa40499e9fe87a2c03
Aryn Folkard <aryn.folkard@null.net>
2021-02-13 22:03:09 +0000

    add google command

---
+++ src/main/java/me/arynxd/monkebot/commands/maincommands/misc/GoogleCommand.java
@@ -0,0 +1,62 @@
+package me.arynxd.monkebot.commands.maincommands.misc;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.function.Consumer;
+import me.arynxd.monkebot.objects.command.Command;
+import me.arynxd.monkebot.objects.command.CommandEvent;
+import me.arynxd.monkebot.objects.exception.CommandException;
+import me.arynxd.monkebot.objects.exception.CommandResultException;
+import me.arynxd.monkebot.util.CommandChecks;
+import org.jetbrains.annotations.NotNull;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+
+@SuppressWarnings("unused")
+public class GoogleCommand extends Command
+{
+	private static final ExecutorService GOOGLE_EXECUTOR = Executors.newCachedThreadPool();
+	public GoogleCommand()
+	{
+		super("Google", "Searches Google.", "[query]");
+		addAliases("google", "g");
+		setCooldown(5000L);
+	}
+
+	@Override
+	public void run(@NotNull List<String> args, @NotNull CommandEvent event, @NotNull Consumer<CommandException> failure)
+	{
+		if(CommandChecks.argsEmpty(event, failure)) return;
+
+		GOOGLE_EXECUTOR.submit(() -> //We require an executor as Jsoup is sync only
+		{
+			try
+			{
+				Document doc = Jsoup.connect("https://www.google.com/search?q=" + String.join("%20", args)).get();
+
+				Elements links = doc.select(".yuRUbf").select("a"); //Select the first google link
+				Elements names = doc.select(".yuRUbf").select("a").select("h3").select("span"); //Select its 'name'
+				Elements descriptions = doc.select(".IsZvec").select("div").select(".aCOpRe").select("span"); //Select its description
+
+				if(links.isEmpty() || names.isEmpty() || descriptions.isEmpty())
+				{
+					failure.accept(new CommandResultException("No results found."));
+					return;
+				}
+
+				event.getChannel().sendMessage(
+						links.get(0).attr("href") + //Grab the actual URL from the 'a' tag
+								" --> *" + names.get(0).text() +
+								"* \n" + descriptions.get(0).text()).queue();
+			}
+			catch(Exception exception)
+			{
+				failure.accept(new CommandResultException("Google search failed!"));
+				event.getMonke().getLogger().error("A Google search exception as occurred", exception);
+			}
+		});
+	}
+}
